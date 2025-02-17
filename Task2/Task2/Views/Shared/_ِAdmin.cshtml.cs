9ea3908fc1bd3eedaadba841cc9b4ef77ba79ee9
Firas-AbuMalloh <firas1995f1995@gmail.com>
2025-02-17 22:36:10 +0300

    Task2

---
+++ Task2/Task2/Views/Shared/_ِAdmin.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Task2.Views.Shared
+{
+    public class _ِAdminModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
